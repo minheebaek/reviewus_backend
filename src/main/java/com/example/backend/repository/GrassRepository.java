14a4d1f62c5b61c8d4e210a90258064ff6918704
minhee <bsmq100@gmail.com>
2024-01-04 22:13:57 +0900

    refactor:불필요한 어노테이션 삭제

--- src/main/java/com/example/backend/repository/GrassRepository.java
+++ src/main/java/com/example/backend/repository/GrassRepository.java
@@ -2,7 +2,6 @@ package com.example.backend.repository;
 
 import com.example.backend.entity.GrassEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -14,7 +13,6 @@ public interface GrassRepository extends JpaRepository<GrassEntity,Long> {
     GrassEntity findByUserIdAndGrassDate(Long userId, String grassDate);
 
     @Transactional
-    @Modifying
     @Query(value = "SELECT g FROM Grass g WHERE g.userId=:userId and (g.grassDate >= :startDate and g.grassDate <= :endDate) order by g.grassDate asc")
     List<GrassEntity> findAllGrassList(Long userId, String startDate, String endDate);
 
--- src/main/java/com/example/backend/repository/SearchLogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.entity.SearchLogEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SearchLogRepository extends JpaRepository<SearchLogEntity, Integer> {
-}
