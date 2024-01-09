eb951003c5e5bb5fd35a2b07dba611738501c10f
minhee <bsmq100@gmail.com>
2024-01-09 20:53:19 +0900

    remove:OAuth Google refresh Token 삭제

--- src/main/java/com/example/backend/dto/object/SocialOAuthTokenDto.java
+++ src/main/java/com/example/backend/dto/object/SocialOAuthTokenDto.java
@@ -11,7 +11,6 @@ import lombok.Setter;
 public class SocialOAuthTokenDto {
     private String access_token;
     private String expires_in;
-    private String refresh_token;
     private String scope;
     private String token_type;
 
